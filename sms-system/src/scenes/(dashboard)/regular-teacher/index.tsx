4e46d8cc1569a2cee4426bbcd9e86f67b3a037ed
TechDev Khamisi <khamisilawrencejm@gmail.com>
2026-05-27 12:34:00 -0500

    feat(teacher): add RegularTeacherPage component with schedule and announcements

---
+++ sms-system/src/scenes/(dashboard)/regular-teacher/index.tsx
@@ -0,0 +1,22 @@
+import Announcements from "@/components/Announcements";
+import BigCalendar from "@/components/BigCalender";
+
+const RegularTeacherPage = () => {
+  return (
+    <div className="p-4 grid grid-cols-12 gap-4">
+      {/* MAIN: Schedule */}
+      <div className="col-span-12 lg:col-span-8">
+        <div className="bg-white dark:bg-gray-800 p-4 rounded-md h-full">
+          <h1 className="text-xl font-semibold">Schedule</h1>
+          <BigCalendar />
+        </div>
+      </div>
+      {/* RIGHT RAIL */}
+      <div className="col-span-12 lg:col-span-4 flex flex-col gap-4">
+        <Announcements />
+      </div>
+    </div>
+  );
+};
+
+export default RegularTeacherPage;
